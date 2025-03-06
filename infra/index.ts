ddf1d4ac91129000bb8d76ac8efeba49f6c13709
Ariel Evangelista <ariel.evangelista@outlook.com>
2025-03-06 13:14:08 -0300

    chore: create and deploy image to ecr.

--- infra/index.ts
+++ infra/index.ts
@@ -1,5 +1,26 @@
-// import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build";
 
-const repository = new awsx.ecr.Repository("deploy-ecr-repo");
+const { repository } = new awsx.ecr.Repository("deploy-ecr-repo");
+
+const { userName, password } = aws.ecr.getAuthorizationTokenOutput({
+  registryId: repository.registryId,
+});
+
+const image = new docker.Image("deploy-docker-image", {
+  context: {
+    location: "../",
+  },
+  platforms: ["linux/amd64"],
+  push: true,
+  registries: [
+    {
+      address: repository.repositoryUrl,
+      username: userName,
+      password,
+    },
+  ],
+  tags: [pulumi.interpolate`${repository.repositoryUrl}:latest`],
+});
